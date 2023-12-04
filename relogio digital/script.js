d0459f4500fd60089b1b6519e0824a3a48ddbc99
Davi Vinicius <davivinicius045@gmail.com>
2023-12-04 12:36:24 -0300

    alterações

--- relogio digital/script.js
+++ relogio digital/script.js
@@ -1,9 +1,20 @@
+let resultado = document.querySelector('.result');
+
+
 function relogio(){
 let d = new Date();
 let h = d.getHours();
 let m = d.getMinutes();
 let s = d.getSeconds();
  
+
+if(h >= 0 && h < 12){
+   resultado.innerHTML = 'Bom dia'
+}else if(h >= 12 && h < 18 ){
+  resultado.innerHTML = 'Boa tarde'
+}else if( h >= 18 && h < 23){
+    resultado.innerHTML = 'Boa noite'
+}
 document.querySelector('.horas').innerHTML =  toFixedZero(h) ;
 document.querySelector('.minutos').innerHTML = toFixedZero(m);
 document.querySelector('.segundos').innerHTML = toFixedZero(s);
